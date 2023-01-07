81ca632784c710fe0cb5530e4910448489b00e66
abukhari148 <m.ali.bukhari148@gmail.com>
2023-01-07 21:11:44 +0100

    bug: get.get_public_tests()

--- jutged/get.py
+++ jutged/get.py
@@ -1,9 +1,17 @@
 def get_public_tests(soup):
     parsed_cases = dict()
-    cases = soup.find_all(class_="horizontal-view")
-    for i, case in enumerate(cases):
-        case = list(case.stripped_strings)
-        parsed_cases[i] = {"input": case[1], "output": case[3]}
+    if soup.find("horizontal-view"):
+        cases = soup.find_all(class_="horizontal-view")
+        for i, case in enumerate(cases):
+            case = list(case.stripped_strings)
+            parsed_cases[i] = {"input": case[1], "output": case[3]}
+    else:
+        #FIXME: adapt special cases such as P96275,P12509 
+        cases = soup.find_all("li", class_="list-group-item")
+        for i, case in enumerate(cases):
+            case = list(case.stripped_strings)
+            if not 'Input/Output':
+                parsed_cases[i] = {"input": case[1], "output": case[3]}
     return parsed_cases
 
 
